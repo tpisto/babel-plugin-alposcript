b6d1091109344bb39ba21e7b0773a1d9ddf4357b
Tommi Pisto <tommi@pis.to>
2022-05-12 12:50:23 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const alpoScriptParser = require("alposcript");
+const path = require("path");
+
+module.exports = function (args) {
+  return {
+    parserOverride(code, opts, babelParser) {
+      let parsedPath = path.parse(opts.sourceFileName);
+      if (parsedPath.ext == ".as") {
+        return alpoScriptParser(code, opts);
+      } else if (parsedPath.ext == ".asx") {
+        return alpoScriptParser(code, { ...opts, ...{ isAsx: true } });
+      } else {
+        return babelParser(code, opts);
+      }
+    },
+  };
+};
